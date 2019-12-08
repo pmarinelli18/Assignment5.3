d05ac149cbcd1f7a9b37fdd72d8e742a207bbf19
pmarinelli18 <pmarinelli@ufl.edu>
2019-12-08 16:43:23 -0500

    init

---
+++ Homepage.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Homepage.aspx.cs" Inherits="Assignment5._3.Homepage" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Query" />
+            <br />
+            <br />
+            <asp:Button ID="insertButton" runat="server" OnClick="Button2_Click" Text="Insert" />
+            <br />
+            <br />
+            <asp:Button ID="updateButton" runat="server" Text="Update" />
+            <br />
+            <br />
+            <asp:DropDownList ID="flowerList" runat="server">
+            </asp:DropDownList>
+            <br />
+            <br />
+            <asp:Button ID="enter" runat="server" Text="Enter" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ Homepage.aspx.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SQLite;
+
+namespace Assignment5._3
+{
+    public partial class Homepage : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            SQLiteConnection myConnection = new SQLiteConnection("Data Source=C:\\flowers2019.db; Version=3");
+            myConnection.Open();
+            //dsff
+            string getFlowers =
+                "SELECT comname " +
+                "FROM flowers";
+
+            string getSize =
+                "SELECT Count(comname) AS countX " +
+                "FROM flowers ";
+
+            // Execute query on database
+            SQLiteCommand selectCommand = new SQLiteCommand(getFlowers, myConnection);
+            SQLiteDataReader dataReader = selectCommand.ExecuteReader();
+
+            SQLiteCommand selectCommandSize = new SQLiteCommand(getSize, myConnection);
+            SQLiteDataReader dataReaderSize = selectCommandSize.ExecuteReader();
+
+            // Iterate every record in the AppUser table
+            while (dataReaderSize.Read())
+            flowerList.TabIndex = dataReaderSize.GetInt16(0);
+            while (dataReader.Read())
+            {
+                flowerList.DataValueField = dataReader.GetString(0) + "\n";
+                //("Flower: " + dataReader.GetString(0));
+            }
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
